1395c6f43b713e12a793075c34501aab7d106126
rastoliska25 <rasto.liska25@gmail.com>
2022-09-21 21:31:34 +0200

    overview authentication

--- src/main/java/signaturePdf/signaturePdfApp/SecurityConfiguration.java
+++ src/main/java/signaturePdf/signaturePdfApp/SecurityConfiguration.java
@@ -6,16 +6,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
+//@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/overview").permitAll()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/overview").authenticated()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
