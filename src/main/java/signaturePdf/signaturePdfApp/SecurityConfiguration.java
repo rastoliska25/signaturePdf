4d46e0b0a0034df30e4df8fd484d38dbf337603c
rastoliska25 <rasto.liska25@gmail.com>
2022-09-22 09:33:51 +0200

    authentication for overview

--- src/main/java/signaturePdf/signaturePdfApp/SecurityConfiguration.java
+++ src/main/java/signaturePdf/signaturePdfApp/SecurityConfiguration.java
@@ -1,11 +1,11 @@
 package signaturePdf.signaturePdfApp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
+//@EnableWebSecurity
 //@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
